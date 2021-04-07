acca778683bdac8035d6afcc464f84840c8a0a54
Erik Mattfolk <erik.mattfolk@gmail.com>
2021-04-07 14:36:06 +0200

    Add documentation + cleanup

--- src/storage/context.tsx
+++ src/storage/context.tsx
@@ -27,32 +27,70 @@ const contextDefault: StorageContextType = {
   setStoreItem: (key: string, value: any) => {},
 };
 
+/**
+ * The context where data is kept at runtime. All data in this context has
+ * a duplicate in AsyncStorage which is loaded when creating the provider.
+ *
+ * @remarks
+ * This context should not be used directly. Use the provided hooks
+ * useActivities, usePeople and so on instead.
+ */
 export const StorageContext = createContext<StorageContextType>(contextDefault);
 
+/**
+ * A context provider which wraps StorageContext.Provider with additional
+ * functionality.
+ *
+ * @remarks
+ * It is undefined behaviour to have more than one of these in the application.
+ *
+ * @example
+ * Wrap a component with the provider:
+ * ```
+ * import { Provider as StorageProvider } from 'storage/context';
+ *
+ * ...
+ *
+ * <StorageProvider>
+ *   <RootNavigator />
+ * </StorageProvider>
+ * ```
+ */
 export const Provider = (props: any) => {
   const [store, setStore] = useState(contextDefault.store);
 
+  /**
+   * Inserts key-value pairs into both AsyncStorage and the store kept in the
+   * context.
+   *
+   * @param key - The key in both AsyncStorage and the store to be updated
+   * @param value - The object to set as the value
+   */
   const setStoreItem = (key: string, value: any) => {
     AsyncStorage.setItem(key, JSON.stringify(value));
     setStore({ ...store, [key]: value });
   }
 
-  // Load data from AsyncStorage
   useEffect(() => {
     (async () => {
-      const activities = await AsyncStorage.getItem(activitiesKey);
-      const calendar = await AsyncStorage.getItem(calendarKey);
-      const icons = await AsyncStorage.getItem(iconsKey);
-      const people = await AsyncStorage.getItem(peopleKey);
-      const settings = await AsyncStorage.getItem(settingsKey);
-      const values = await AsyncStorage.getItem(valuesKey);
+      /**
+       * Returns a value from AsyncStorage if it exists, def(ault) otherwise.
+       *
+       * @param key - The key of the desired value in AsyncStorage
+       * @param def - The object to use if no value is found
+       * @returns A value from AsyncStorage if it exists, def otherwise.
+       */
+      const getFromStorage = async (key: string, def: any): any => {
+        const value = await AsyncStorage.getItem(key);
+        return value ? JSON.parse(value) : def;
+      }
       const newStore = {
-        [activitiesKey]: activities ? JSON.parse(activities) : activitiesDefault,
-        [calendarKey]: calendar ? JSON.parse(calendar) : calendarDefault,
-        [iconsKey]: icons ? JSON.parse(icons) : iconsDefault,
-        [peopleKey]: people ? JSON.parse(people) : peopleDefault,
-        [settingsKey]: settings ? JSON.parse(settings) : settingsDefault,
-        [valuesKey]: values ? JSON.parse(values) : valuesDefault,
+        [activitiesKey]: await getFromStorage(activitiesKey, activitiesDefault),
+        [calendarKey]: await getFromStorage(calendarKey, calendarDefault),
+        [iconsKey]: await getFromStorage(iconsKey, iconsDefault),
+        [peopleKey]: await getFromStorage(peopleKey, peopleDefault),
+        [settingsKey]: await getFromStorage(settingsKey, settingsDefault),
+        [valuesKey]: await getFromStorage(valuesKey, valuesDefault),
       };
       setStore(newStore);
     })()
