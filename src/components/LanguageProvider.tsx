77d7abf87800237ed1099cdf4f559057ace90c7f
Daniel Karlsson <danier1999@gmail.com>
2021-04-08 13:43:19 +0200

    Language: added comments and dictionary standard
    Co-authored-by: augustcarlson <augustcarlson@users.noreply.github.com>

--- src/components/LanguageProvider.tsx
+++ src/components/LanguageProvider.tsx
@@ -7,6 +7,9 @@ import Storage from '../Storage'
 
 const LanguageContext = createContext(sv);
 
+/**
+ * Provides the current language dictionary to its children.
+ */
 export const LanguageProvider: React.FC = ({children}) => {
   const [settings, modifySettings] = Storage.useSettings();
   const dict = languages[settings.language];
@@ -18,5 +21,20 @@ export const LanguageProvider: React.FC = ({children}) => {
   );
 };
 
-//TODO: Replace any
+/**
+ * Function returning the text from the correct language dictionary.
+ * @param entry keyvalue for the entry in the current language json file
+ * @returns translation/value for the current language
+ * 
+ * @example
+ * Get the welcome message for the current language
+ * getTranslation("valuesHeaderEvaluation");
+ * returns "Värdering" if swedish is the current language
+ * ```
+ * keyvalues can be found in the respective json, and new translations should
+ * be added in every file (src/language).
+ * 
+ * TODO: Fix types and replace any
+ * ```
+ */
 export const getTranslation = (entry: string) => useContext<any>(LanguageContext)[entry];
--- src/language/index.tsx
+++ src/language/index.tsx
@@ -1,6 +1,17 @@
 import en from './en.json';
 import sv from './sv.json';
+//"valuesHeaderEvaluation"
+//"activitiesButtonGradeDay"
+//"activitiesSliderGradeDay"
+//Format: "navigationbarTypeText"
+//navigationbar: activities, values, calendar, info
 
+//"january"
+//"february"
+//.
+//.
+//.
+//"monday"
 interface Languages{
     [index: string]: any;
 }
