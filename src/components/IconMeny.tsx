cc0ef49ec35837420d7bbd52839ba1c86f064f50
malin-mallan <78034678+malin-mallan@users.noreply.github.com>
2021-04-08 10:22:25 +0200

    Update src/components/IconMeny.tsx
    Co-authored-by: Erik Mattfolk <erik.mattfolk@gmail.com>

--- src/components/IconMeny.tsx
+++ src/components/IconMeny.tsx
@@ -27,10 +27,10 @@ export const IconMeny = (props: any) => {
   return (
     <View style={{ flexGrow: 1, flexDirection: 'row', alignItems: 'stretch', justifyContent: 'center', paddingRight: 20, paddingLeft: 20 }}>
       <View style={{ flex: 1, justifyContent: 'space-evenly', alignItems: 'center' }}>
-        <IconButton icon={icons[0]} size={iconSize} onPress={() => { props.pressCallback(0, icons[0]) }} />
-        <IconButton icon={icons[1]} size={iconSize} onPress={() => { props.pressCallback(1, icons[1]) }} />
-        <IconButton icon={icons[2]} size={iconSize} onPress={() => { props.pressCallback(2, icons[2]) }} />
-        <IconButton icon={icons[3]} size={iconSize} onPress={() => { props.pressCallback(3, icons[3]) }} />
+        <IconButton icon={icons[0 + 0 * 4]} size={iconSize} onPress={() => { props.pressCallback(0, icons[0 + 0 * 4]) }} />
+        <IconButton icon={icons[1 + 0 * 4]} size={iconSize} onPress={() => { props.pressCallback(1, icons[1 + 0 * 4]) }} />
+        <IconButton icon={icons[2 + 0 * 4]} size={iconSize} onPress={() => { props.pressCallback(2, icons[2 + 0 * 4]) }} />
+        <IconButton icon={icons[3 + 0 * 4]} size={iconSize} onPress={() => { props.pressCallback(3, icons[3 + 0 * 4]) }} />
       </View>
       <View style={{ flex: 1, justifyContent: 'space-evenly', alignItems: 'center' }}>
         <IconButton icon={icons[0 + 1 * 4]} size={iconSize} onPress={() => { props.pressCallback(0 + 1 * 4, icons[0 + 1 * 4]) }} />
