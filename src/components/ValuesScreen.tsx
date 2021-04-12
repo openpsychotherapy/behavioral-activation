09ea1c03925f9d32033cb17cfd4394b62edac9fb
Lukas Jonsson <lullejonsson@gmail.com>
2021-04-12 12:37:34 +0200

    started on values

--- src/components/ValuesScreen.tsx
+++ src/components/ValuesScreen.tsx
@@ -1,28 +1,49 @@
 import React from 'react';
-import { View } from 'react-native';
-import { Text } from 'react-native-paper';
+import { View, StyleSheet } from 'react-native';
+import { Text, Title, List} from 'react-native-paper';
 
 import { createStackNavigator } from '@react-navigation/stack';
 import { CustomNavigationBar } from './CustomNavigationBar';
+import { FlatList } from 'react-native-gesture-handler';
 
 const ValuesStack = createStackNavigator();
 
-const ViewContent = () => {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Values</Text>
-    </View>
-  );
-}
+/*const ViewTitle = () => (
+  <View style={{ flex: 1, justifyContent: 'flex-start', alignItems: 'center'}}>
+    <Title>Värdering</Title>
+  </View>
+)*/
+
+const ValuesList = () => (
+  <View style={{ flex: 1, justifyContent: 'flex-start', alignItems: 'center'}}>
+    <List.Section>
+      <List.Subheader>Värdering</List.Subheader>
+      <List.Item title="Relation" />
+      <List.Item title="Studier/Karriär" />
+      <List.Item title="Fritid/Intressen" />
+      <List.Item title="Sinne/Kropp/Spirituellt" />
+      <List.Item title="Dagligt ansvar" /> 
+
+   </List.Section>
+  </View>
+  
+);
+
+
+
+
+
+  
 
 export const ValuesScreen = () => {
   return (
     <ValuesStack.Navigator initialRouteName="Values" headerMode="float"
       screenOptions={{
-        header: (props) => <CustomNavigationBar {...props} />,
+        header: (props: any) => <CustomNavigationBar {...props} />,
       }}
     >
-      <ValuesStack.Screen name="Values" component={ViewContent} />
+      <ValuesStack.Screen name="ValuesList" component={ValuesList} />
+
     </ValuesStack.Navigator>
   );
 }
