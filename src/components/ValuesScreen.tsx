b138a3885b7840cc1aa66c9abaf258df175ba034
Lukas Jonsson <lullejonsson@gmail.com>
2021-04-13 12:02:09 +0200

    start screen on values is hopefully done

--- src/components/ValuesScreen.tsx
+++ src/components/ValuesScreen.tsx
@@ -8,69 +8,101 @@ import { FlatList } from 'react-native-gesture-handler';
 
 const ValuesStack = createStackNavigator();
 
-const TitleText = () => (
-    <Title>Värdering</Title>
-)
-
-const RelationButton = () => (
-
-  <Button mode="outlined" onPress={() => console.log('Pressed')}>
-    Relation
-  </Button>
-
-)
-const StudierButton = () => (
-      <Button mode="outlined" onPress={() => console.log('Pressed')}>
-    Studier/Karriär
-  </Button>
-
-
-)
-const FritidButton = () => (
-      <Button mode="outlined" onPress={() => console.log('Pressed')}>
-    Fritid/intressen
-  </Button>
-)
-
-const ViewContent = () => (
-  <View style={{flex: 1}}>
-    <View style={{flex: 0.14, alignItems: 'center', justifyContent: 'center'}}>
-    <Title>Värdering</Title>
-    </View>
-    <View style={{flex: 0.14, alignItems: 'center', justifyContent: 'center'}}>
-      <Button mode="outlined" onPress={() => console.log('Pressed')}>
-        Relation
-      </Button>
-    </View>
-    <View style={{ flex: 0.14,  alignItems: 'center', justifyContent: 'center'}}>
-      <Button mode="outlined" onPress={() => console.log('Pressed')}>
-        Studier/Karriär
-      </Button>
-    </View>
-    <View style={{flex: 0.14, alignItems: 'center', justifyContent: 'center'}}>
-      <Button mode="outlined" onPress={() => console.log('Pressed')}>
-        Fritid/intressent
-      </Button>
-    </View>
-    <View style={{flex: 0.14, alignItems: 'center', justifyContent: 'center'}}>
-      <Button mode="outlined" onPress={() => console.log('Pressed')}>
-        Sinne/Kropp/Spirituellt
-      </Button>
-    </View>
-    <View style={{flex: 0.14, alignItems: 'center', justifyContent: 'center'}}>
-      <Button mode="outlined" onPress={() => console.log('Pressed')}>
-        Dagliga ansvar
-      </Button>
-    </View>
-    <View style={{flex: 0.05, alignItems: 'center', justifyContent: 'center'}}>
-    </View>
-    <View style={{flex: 0.14, alignItems: 'center', justifyContent: 'center'}}>
-      <Button mode="outlined" onPress={() => console.log('Pressed')}>
-        Stödpersoner
-      </Button>
-    </View>
+const RelationView = () => (
+  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Text>Relation</Text>
   </View>
-)
+); 
+
+const CareerView = () => (
+  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Text>Studier/karrriär</Text>
+  </View>
+); 
+
+const InterestsView = () => (
+  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Text>Fritid/intressen</Text>
+  </View>
+);
+
+const MindView = () => (
+  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Text>Sinne/Kropp/Spirituellt</Text>
+  </View>
+);
+
+const ResposibilityView = () => (
+  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Text>Dagliga ansvar</Text>
+  </View>
+);
+
+const SupportView = () => (
+  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Text>Stödpersoner</Text>
+  </View>
+);
+
+
+const CreateButton = (props: any) => {
+  return (
+    <View style={{flex: 0.14, alignItems: 'center', justifyContent: 'center'}}>
+      <Button theme={{ roundness: 30 }} contentStyle={{width: 240, height: 70}} compact={true} mode="outlined" onPress={props.function}>
+        <Text>{props.name}</Text>
+      </Button>
+    </View>
+      )
+}
+
+
+  
+const ViewContent = ({navigation}: any) => {
+  
+  const relationButton = () => {
+    navigation.navigate('Relation');
+  };
+
+  const careerButton = () => {
+    navigation.navigate('Career');
+  };
+
+  const interestsButton = () => {
+    navigation.navigate('Interests');
+  };
+
+  const mindButton = () => {
+    navigation.navigate('Mind');
+  };
+
+  const responsibilityButton = () => {
+    navigation.navigate('Responsibility');
+  };
+
+  const supportButton = () => {
+    navigation.navigate('Support');
+  };
+
+
+  return (
+    <View style={{flex: 1}}>
+    <View style={{flex: 0.10, alignItems: 'center', justifyContent: 'center'}}>
+      <Title>Värdering</Title>
+    </View>
+    <CreateButton name="relation" function={relationButton}/>
+    <CreateButton name="Studier/karriär" function={careerButton}/>
+    <CreateButton name="Fritid/intressen" function={interestsButton}/>
+    <CreateButton name="Sinne/kropp/spirituellt" function={mindButton}/>
+    <CreateButton name="Dagliga ansvar" function={responsibilityButton}/>
+    <View style={{flex: 0.02, alignItems: 'center', justifyContent: 'center'}}>
+    </View>
+    <CreateButton name="Stödpersoner" function={supportButton}/>
+  </View>
+  )
+}
+
+
+
 
 
   
@@ -82,8 +114,13 @@ export const ValuesScreen = () => {
         header: (props: any) => <CustomNavigationBar {...props} />,
       }}
     >
-      <ValuesStack.Screen name="RelationButton" component={ViewContent} />
-      
+      <ValuesStack.Screen name="ViewContent" component={ViewContent} />
+      <ValuesStack.Screen name="Relation" component={RelationView} />
+      <ValuesStack.Screen name="Career" component={CareerView} />
+      <ValuesStack.Screen name="Interests" component={InterestsView} />
+      <ValuesStack.Screen name="Mind" component={MindView} />
+      <ValuesStack.Screen name="Responsibility" component={ResposibilityView} />
+      <ValuesStack.Screen name="Support" component={SupportView} />
 
     </ValuesStack.Navigator>
   );
