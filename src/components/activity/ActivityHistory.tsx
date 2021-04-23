2380723f2ab52b98e7f20083ffc35d274a82225d
Neathan <jonathanohrlingg@gmail.com>
2021-04-23 10:02:23 +0200

    more wip.

--- src/components/activity/ActivityHistory.tsx
+++ src/components/activity/ActivityHistory.tsx
@@ -30,17 +30,30 @@ export const ActivityHistory = () => {
   if (activities.length !== 0) {
     const day = activities[currentDay];
 
-    let lastActivity = null; // Used for merging activities
+    let activityCount = 0; // Used for merging activities
     for (let activityIndex = 0; activityIndex < day.entries.length; ++activityIndex) {
       const activity = day.entries[activityIndex];
-      if (!activity || lastActivity === activity) continue;
-      lastActivity = activity;
-      const date = getDateFromStringWithOffset(day.date, activityIndex);
-      const timeString = getFormattedTime(date);
+      
+      const outside = activityIndex+1-day.entries.length === 0;
+      
+      // Should check if the comming activity is null not the current.
+      const shouldBreak = !activity || outside || (!outside && activity !== day.entries[activityIndex+1]);
+
+      if (!shouldBreak) {
+        // Don't break streak
+        ++activityCount;
+        continue;
+      }
+
+      // Apply segment
+      const fromDate = getDateFromStringWithOffset(day.date, activityIndex-activityCount);
+      const toDate = getDateFromStringWithOffset(day.date, activityIndex);
+      const fromTimeString = getFormattedTime(fromDate);
+      const toTimeString = getFormattedTime(toDate);
 
       historyItems.push(
-        <Surface key={'hi_' + date.toString()} style={{ flexDirection: 'row', borderRadius: 5, elevation: 5, marginHorizontal: 10 , marginVertical: 5 }}>
-          <List.Item style={{flex: 1, flexGrow: 1 }} title={timeString} description={activity.text} right={() =>
+        <Surface key={'hi_' + fromDate.toString()} style={{ flexDirection: 'row', borderRadius: 5, elevation: 5, marginHorizontal: 10 , marginVertical: 5 }}>
+          <List.Item style={{flex: 1, flexGrow: 1 }} title={fromTimeString + ' - ' + toTimeString} description={activity.text} right={() =>
             <View style={{flexDirection: 'row', alignItems: 'center'}}>
               <Caption>{activity.importance + ' | ' + activity.enjoyment}</Caption>
               <List.Icon icon={activity.icon}/>
@@ -48,6 +61,9 @@ export const ActivityHistory = () => {
           }/>
         </Surface>
       );
+      
+      // Reset segment
+      activityCount = 0;
     }
   }
   else {
